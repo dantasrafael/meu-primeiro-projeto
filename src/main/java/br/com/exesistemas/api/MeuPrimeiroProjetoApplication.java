5f7588a057e33a57191946aec1167f7b98abc1fc
Rafael Dantas <rafael.dantas.exesistemas@gmail.com>
2018-03-02 13:02:27 -0300

    Conclusão Aula 14

--- src/main/java/br/com/exesistemas/api/MeuPrimeiroProjetoApplication.java
+++ src/main/java/br/com/exesistemas/api/MeuPrimeiroProjetoApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import br.com.exesistemas.api.utils.SenhaUtils;
+
 @SpringBootApplication
 public class MeuPrimeiroProjetoApplication {
 	
@@ -20,6 +22,14 @@ public class MeuPrimeiroProjetoApplication {
 	public CommandLineRunner commandLineRunner() {
 		return args -> {
 			System.out.println("### Quantidade de registros por página = " + this.qtdPorPagina);
+			
+			String senhaEncoded = SenhaUtils.gerarBCrypt("123456");
+			System.out.println("Senha encoded = " + senhaEncoded);
+			
+			senhaEncoded = SenhaUtils.gerarBCrypt("123456");
+			System.out.println("Senha encoded 2 = " + senhaEncoded);
+			
+			System.out.println("Senha valida = " + SenhaUtils.senhaValida("123456", senhaEncoded));
 		};
 	}
 }
