7d58007ab98c596fe06916fb812efa1a2d49d641
Rafael Dantas <dantasrafael@yahoo.com.br>
2018-03-01 22:06:02 -0300

    Estudo até a aula 14

--- src/main/java/br/com/exesistemas/api/MeuPrimeiroProjetoApplication.java
+++ src/main/java/br/com/exesistemas/api/MeuPrimeiroProjetoApplication.java
@@ -1,12 +1,25 @@
 package br.com.exesistemas.api;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class MeuPrimeiroProjetoApplication {
+	
+	@Value("${paginacao.qtd_por_pagina}")
+	private int qtdPorPagina;
 
 	public static void main(String[] args) {
 		SpringApplication.run(MeuPrimeiroProjetoApplication.class, args);
+	}
+	
+	@Bean
+	public CommandLineRunner commandLineRunner() {
+		return args -> {
+			System.out.println("### Quantidade de registros por página = " + this.qtdPorPagina);
+		};
 	}
 }
